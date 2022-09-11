621ec03a3ce54278f781b8512a96d0bb1676d508
julioretkwa <julio@zoocha.com>
2022-09-11 12:21:16 -0300

    estilizando lsita

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Counter } from './components/Counter';
+
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss'; 
 
@@ -7,10 +7,7 @@ export function App() {
 //throw new Error('Forninho caiu.');
 
     return (
-        <>
             <RepositoryList/>
-            <Counter/>
-        </>
     
     );
 }
