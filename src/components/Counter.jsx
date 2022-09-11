6767b9992ddeffc6cd4e20a254e0339913310061
julioretkwa <julio@zoocha.com>
2022-09-11 12:00:04 -0300

    fast refresh react webpack

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -12,7 +12,7 @@ export function Counter() {
         <div>
             <h2>{counter}</h2>
             <button type="button" onClick={increment}>
-                Increment
+                nsei +1
             </button>
         </div>
     )
